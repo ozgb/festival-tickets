77550ae6f6d66e67f67e7e6ff4bea26157c600de
Oscar Bailey <oscarbailey@thorncontractors.co.uk>
2024-02-02 14:22:50 +0000

    build: add vue to frontend

--- frontend/src/client.ts
+++ frontend/src/client.ts
@@ -1,13 +1,26 @@
 import {GetOrderResponse, GetOrderRequest} from '../grpc/purchase_pb.js';
 import {ProductServiceClient} from '../grpc/PurchaseServiceClientPb';
 import * as grpcWeb from "grpc-web";
+import {createApp, ref} from "vue";
 
 var client = new ProductServiceClient('http://localhost:8080', {}, {});
 
 var request = new GetOrderRequest();
 request.setId('2a64a953-515e-4b77-90e2-4250be54df67');
 
-client.getOrder(request, {}, (err: grpcWeb.RpcError, response: GetOrderResponse) => {
-  let order = response.getOrder();
-  console.log(order.getId());
-});
+
+createApp({
+  setup() {
+    const message = ref('Hello vue!')
+
+    client.getOrder(request, {}, (err: grpcWeb.RpcError, response: GetOrderResponse) => {
+      let order = response.getOrder();
+      message.value = order.getId();
+      //console.log(order.getId());
+    });
+
+    return {
+      message
+    }
+  }
+}).mount('#app')
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,19 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/client.ts',
+  plugins: [
+    // Define Vue feature flags
+    new webpack.DefinePlugin({
+      // Enable or disable Vue's options API
+      __VUE_OPTIONS_API__: JSON.stringify(true),
+      // Enable or disable Vue devtools in production
+      __VUE_PROD_DEVTOOLS__: JSON.stringify(false),
+      // Provide details on hydration mismatches in production
+      __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(false),
+    }),
+  ],
   module: {
     rules: [
       {
@@ -12,6 +24,9 @@ module.exports = {
     ],
   },
   resolve: {
+    alias: {
+      'vue': "vue/dist/vue.esm-bundler.js",
+    },
     extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
